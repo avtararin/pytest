87c7132b29c566c4e95471bd7c0a29fb8ac11692
Aleksandr Tararin <truckjyn@student.21-school.ru>
2023-10-11 09:30:48 +0300

    I learned how to group tests and catch errors

--- src/main.py
+++ src/main.py
@@ -2,9 +2,15 @@ from typing import Union
 
 class Calculator:
     def divide(self, x:Union[int, float], y:Union[int, float]) -> int | float:
+        if not isinstance(x, (int, float)) or not isinstance(y, (int, float)):
+            raise TypeError("Both arguments must be numeric")
+        if y == 0:
+            raise ZeroDivisionError("Cannot divide by zero")
         return x / y
 
     def add (self, x:Union[int, float], y:Union[int, float]) -> int | float:
+        if not isinstance(x, (int, float)) or not isinstance(y, (int, float)):
+            raise TypeError("Both arguments must be numeric")
         return x + y
 
 if __name__ == "__main__":
